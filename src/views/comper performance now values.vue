ed2dc9dd384a1b8ce7d86890f0053c145013be75
Michael Clegg <michaejclegg@gmail.com>
2023-01-17 23:38:27 +0100

    first commit

---
+++ src/views/comper performance now values.vue
@@ -0,0 +1,51 @@
+<script lang="ts" setup>
+//--------------------------------
+// COMPARE MATH RANDOM VALUES -
+//
+// PERFORMANCE .NOW
+let count = 1;
+function generateValue() {
+  // SET Value  toFixed from 2 - 5
+  let value = Math.random().toFixed(3);
+  // value = Math.round(value)
+  return value;
+}
+
+const compare = () => {
+  let value1 = generateValue();
+  let value2 = generateValue();
+
+  while (value1 !== value2) {
+    let start = performance.now();
+    count++; //?
+    console.log(`Values are not the same: ${value1} ${value2}`);
+    value1 = generateValue();
+    value2 = generateValue();
+  }
+
+  let end = performance.now();
+  let differencePerformance = Math.round(end - start) / 10;
+  const secORmin =
+    differencePerformance >= 60
+      ? `${(differencePerformance / 60).toFixed(1)}  minutes`
+      : `${differencePerformance}  seconds`;
+  console.log(count, secORmin);
+  console.log("Values same after ", count, "times and\n ", secORmin); //
+};
+// NOTE - output after value set to 4 figure
+// values match after 8284 times 6.6 seconds
+
+//loop a few times (5) to compare results
+// nameless IIFE (Immediately Invoked Function Expression)
+var loops = 0;
+(() => {
+  while (loops < 5) {
+    compare();
+    loops++;
+  }
+})();
+</script>
+
+<template></template>
+
+<style scoped></style>
--- src/views/test.vue
+++ src/views/test.vue
@@ -1,7 +1,119 @@
-<script lang="ts">
-const saveFile = () => {
-  const data = JSON.stringify(arr);
-  window.localStorage.setItem("arr", data);
-  console.log(JSON.parse(window.localStorage.getItem("arr")));
+<script lang="ts" setup>
+import { ref } from "vue";
+const isOpen = ref(false);
+let cleggFirstName = ref("mike");
+//--------------------------------
+// COMPARE -
+
+var loops = 0;
+(() => {
+  while (loops < 5) {
+    console.log("number of ", loops);
+    loops++;
+  }
+})();
+
+const handeledMe = () => {
+  console.log("you touched", isOpen.value);
+  isOpen.value = true;
 };
+
+// IIFE;
+(() => {})();
+// or verbose
++(function () {
+  console.log("Unary PLUS operator starts the expression");
+})();
 </script>
+
+<template>
+  <body>
+    <div class="cards">
+      <div
+        class="card"
+        @click="handeledMe"
+        :class="{ open: isOpen }"
+        data-description="Wes is cool"
+      >
+        <img src="https://picsum.photos/200?random=1" alt="Wes Bos" />
+        <h2>Wes Bos</h2>
+        <button>Learn more →</button>
+      </div>
+      <div class="card" @click="handeledMe" data-description="Scott is neat!">
+        <img src="https://picsum.photos/200?random=2" alt="Wes Bos" />
+        <h2>Scott Tolinski</h2>
+        <button>Learn more →</button>
+      </div>
+      <div
+        class="card"
+        @click="handeledMe"
+        data-description="Kait is beautiful!"
+      >
+        <img src="https://picsum.photos/200?random=3" alt="Wes Bos" />
+        <h2>Kait Bos</h2>
+        <button>Learn more →</button>
+      </div>
+      <div
+        class="card"
+        @click="handeledMe"
+        data-description="Snickers is a dog!"
+      >
+        <img src="https://picsum.photos/200?random=4" alt="Wes Bos" />
+        <h2>Snickers the dog</h2>
+        <button>Learn more →</button>
+      </div>
+    </div>
+    <div class="modal-outer">
+      <div class="modal-inner">test 123</div>
+    </div>
+  </body>
+</template>
+
+<style scoped>
+.cards {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-gap: 20px;
+  padding: 2rem;
+}
+.card {
+  background: white;
+  padding: 1rem;
+  border-radius: 2px;
+}
+.card img {
+  width: 100%;
+}
+.card h2 {
+  color: black;
+}
+
+.modal-outer {
+  display: grid;
+  background: hsla(50, 100%, 50%, 0.6);
+  position: fixed;
+  height: 100vh;
+  width: 100vw;
+  top: 0;
+  left: 0;
+  justify-content: center;
+  align-items: center;
+  /* hide this modal until we need it */
+  opacity: 0;
+  pointer-events: none;
+  transition: opacity 0.2s;
+}
+
+.modal-outer .open {
+  opacity: 1;
+  pointer-events: all;
+}
+.modal-inner {
+  max-width: 600px;
+  min-width: 400px;
+  padding: 2rem;
+  border-radius: 5px;
+  min-height: 200px;
+  background: rgb(223, 179, 179);
+}
+</style>
